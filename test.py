97e37565ed5b0f3369674dde3be9c1fdb6122100
MaddhaliManeesh <maneesh@ibhubs.co>
2017-09-16 13:28:21 +0530

    added code documentation

--- test.py
+++ test.py
@@ -1,2 +1,23 @@
-store=[[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]]
-print store[0][0][2]
+class Employee:
+    'Common base class for all employees'
+    empCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+
+    def displayCount(self):
+        print "Total Employee %d" % Employee.empCount
+
+    def displayEmployee(self):
+        print "Name : ", self.name, ", Salary: ", self.salary
+
+
+"This would create first object of Employee class"
+emp1 = Employee("Zara", 2000)
+"This would create second object of Employee class"
+emp2 = Employee("Manni", 5000)
+emp1.displayEmployee()
+emp2.displayEmployee()
+print "Total Employee %d" % Employee.empCount
